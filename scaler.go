c45d60d55a2cec2d8b47743e607dad8d32bf8fa1
Marcus Stong <stongo@Marcuss-MacBook-Pro.local>
2016-09-21 09:35:55 -0400

    more separation of concern

--- scaler.go
+++ scaler.go
@@ -20,21 +20,51 @@ func NewScaler(data *ScalerData) *Scaler {
 	return &Scaler{Data: data}
 }
 
+func (s *Scaler) ScaleUpPrecise() (string, error) {
+	out, err := exec.Command("echo", "scale up precise").Output()
+	if err != nil {
+		log.Print(err)
+		return "", fmt.Errorf("Command response: %s\n", err)
+	}
+	return fmt.Sprintf("Command response: %s\n", out), nil
+}
+
+func (s *Scaler) ScaleUpTrusty() (string, error) {
+	out, err := exec.Command("echo", "scale up trusty").Output()
+	if err != nil {
+		log.Print(err)
+		return "", fmt.Errorf("Command response: %s\n", err)
+	}
+	return fmt.Sprintf("Command response: %s\n", out), nil
+}
+
+func (s *Scaler) ScaleDownPrecise() (string, error) {
+	out, err := exec.Command("echo", "scale down precise").Output()
+	if err != nil {
+		log.Print(err)
+		return "", fmt.Errorf("Command response: %s\n", err)
+	}
+	return fmt.Sprintf("Command response: %s\n", out), nil
+}
+
+func (s *Scaler) ScaleDownTrusty() (string, error) {
+	out, err := exec.Command("echo", "scale down trusty").Output()
+	if err != nil {
+		log.Print(err)
+		return "", fmt.Errorf("Command response: %s\n", err)
+	}
+	return fmt.Sprintf("Command response: %s\n", out), nil
+}
+
 func (s *Scaler) Scale() (string, error) {
-	if strings.Contains(s.Data.EventTitle, "[Scale Up]") {
-		out, commandErr := exec.Command("echo", "hello").Output()
-		if commandErr != nil {
-			log.Print(commandErr)
-			return "", fmt.Errorf("Command response: %s\n", commandErr)
-		}
-		return fmt.Sprintf("Command response: %s\n", out), nil
-	} else if strings.Contains(s.Data.EventTitle, "[Scale Down]") {
-		out, commandErr := exec.Command("echo", "bye").Output()
-		if commandErr != nil {
-			log.Print(commandErr)
-			return "", fmt.Errorf("Command response: %s\n", commandErr)
-		}
-		return fmt.Sprintf("Command response: %s\n", out), nil
+	if strings.Contains(s.Data.EventTitle, "[Scale Up Precise]") {
+		return s.ScaleUpPrecise()
+	} else if strings.Contains(s.Data.EventTitle, "[Scale Up Trusty]") {
+		return s.ScaleUpTrusty()
+	} else if strings.Contains(s.Data.EventTitle, "[Scale Down Precise]") {
+		return s.ScaleDownPrecise()
+	} else if strings.Contains(s.Data.EventTitle, "[Scale Down Trusty]") {
+		return s.ScaleDownTrusty()
 	}
 	return "no command executed", nil
 }
